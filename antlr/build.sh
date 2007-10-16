015e869baedfb8b19249c93b21d84bb76d83e0cb
Wincent Colaiuta <win@wincent.com>
2007-10-16 22:02:47 +0200

    Refactor script into build and install portions
    Now we have a build script (which builds the ANTLR tool and the C target
    runtime on Darwin, and only the runtime on Linux) and a separate install
    script (which installs tool and runtime on Darwin, and only the runtime
    on Linux).
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- antlr/build.sh
+++ antlr/build.sh
@@ -19,25 +19,31 @@ do
   patch -p0 < "${PATCH}"
 done
 
-echo "Building ANTLR..."
-OLD_CLASSPATH="$CLASSPATH"
-export CLASSPATH="/usr/local/junit/junit.jar:./lib/antlr-2.7.7.jar"
-cd "${ANTLR_VERSION}"
-ant -quiet generator
-cd codegen
-FILES=`ls *.java`
-cd ..
-for FILE in $FILES
-do
-  find src/org/antlr -name $FILE -delete
-done
-ant -quiet build
-export CLASSPATH="$OLD_CLASSPATH"
+# build ANTLR itself only on the local Darwin machine
+if [ `uname` = "Darwin" ]; then
+  echo "Building ANTLR..."
+  OLD_CLASSPATH="$CLASSPATH"
+  export CLASSPATH="/usr/local/junit/junit.jar:./lib/antlr-2.7.7.jar"
+  cd "${ANTLR_VERSION}"
+  ant -quiet generator
+  cd codegen
+  FILES=`ls *.java`
+  cd ..
+  for FILE in $FILES
+  do
+    find src/org/antlr -name $FILE -delete
+  done
+  ant -quiet build
+  cp build/antlr.jar lib/antlr-3.0.1-custom.jar
+  mv lib/antlr-3.0.1.jar lib/antlr-3.0.1.jar.disabled
+  export CLASSPATH="$OLD_CLASSPATH"
+  cd ..
+fi
 
 echo "Building C target runtime..."
-cd "runtime/C/dist/libantlr3c-${VERSION}"
+cd "${ANTLR_VERSION}/runtime/C/dist/libantlr3c-${VERSION}"
 ./configure --disable-shared
 make
 make check
 
-echo "Done"
+echo "Done: run ./install.sh to install"
---
+++ antlr/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+VERSION="3.0.1"
+ANTLR_VERSION="antlr-${VERSION}"
+
+echo "Installing C target runtime..."
+cd "${ANTLR_VERSION}/runtime/C/dist/libantlr3c-${VERSION}"
+sudo make install
+cd "../../../../.."
+
+# install ANTLR itself only on the local Darwin machine
+if [ `uname` = "Darwin" ]; then
+  echo "Installing ANTLR..."
+  sudo mv "${ANTLR_VERSION}" /usr/local/
+  sudo rm -f /usr/local/antlr
+  $(cd /usr/local && sudo ln -s "${ANTLR_VERSION}" antlr)
+  echo "To use ANTLR, add the following exports to your environment:"
+  cat <<EXPORTS
+  export CLASSPATH=".:/usr/local/antlr/lib/antlr-3.0.1-custom.jar"
+  export CLASSPATH="\$CLASSPATH:/usr/local/antlr/lib/antlr-2.7.7.jar"
+  export CLASSPATH="\$CLASSPATH:/usr/local/antlr/lib/antlr-runtime-3.0.1.jar"
+  export CLASSPATH="\$CLASSPATH:/usr/local/antlr/lib/stringtemplate-3.1b1.jar"
+EXPORTS
+fi
+
+echo "Done"
