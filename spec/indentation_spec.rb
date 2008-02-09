bf79f031a86d6acadce38288ec743f734063d88c
Wincent Colaiuta <win@wincent.com>
2008-02-09 12:28:45 +0100

    Add indentation specs
    Test user overrides of base indentation. The intention here is for
    incorporation in Haml templates (or any other templating system that
    takes care to produce beautifully-formatted HTML). The user can supply
    a base indentation level that matches the expected indentation level
    in the enclosing Haml template; in this way the wikitext-generated
    portion won't break the nice indentation of the template as a whole.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/indentation_spec.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+# Copyright 2008 Wincent Colaiuta
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require File.join(File.dirname(__FILE__), 'spec_helper.rb')
+require 'wikitext'
+
+describe Wikitext::Parser, 'indentation' do
+  before do
+    @parser         = Wikitext::Parser.new
+    @input          = '* foo'
+    @default_output = <<-END
+<ul>
+  <li>foo</li>
+</ul>
+END
+  end
+
+  it 'should default to no additional indentation' do
+    @parser.parse('* foo').should == @default_output
+  end
+
+  it 'should add additional indentation as indicated by the "indent" option' do
+    # 2 extra spaces
+    @parser.parse('* foo', :indent => 2).should == <<-END
+  <ul>
+    <li>foo</li>
+  </ul>
+END
+
+    # 4 extra spaces
+    @parser.parse('* foo', :indent => 4).should == <<-END
+    <ul>
+      <li>foo</li>
+    </ul>
+END
+
+    # 6 extra spaces
+    @parser.parse('* foo', :indent => 6).should == <<-END
+      <ul>
+        <li>foo</li>
+      </ul>
+END
+  end
+
+  it 'should complain if the "indent" option is nil' do
+    lambda { @parser.parse('* foo', :default => nil) }.should raise_error(TypeError)
+  end
+
+  it 'should complain if the "indent" options is not an integer' do
+    lambda { @parser.parse('* foo', :default => 'bar') }.should raise_error(TypeError)
+    lambda { @parser.parse('* foo', :default => /baz/) }.should raise_error(TypeError)
+  end
+
+  it 'should treat a negative "indent" as though it were zero' do
+    @parser.parse('* foo', :indent => -4).should == @default_output
+  end
+
+  it 'should coerce a float "indent" into an integer' do
+    @parser.parse('* foo', :indent => 0.0).should == @default_output
+    @parser.parse('* foo', :indent => 2.0).should == <<-END
+  <ul>
+    <li>foo</li>
+  </ul>
+END
+  end
+end
