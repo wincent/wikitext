3d507daf4a42b5bcc6905e128bd4b463169d7227
Wincent Colaiuta <win@wincent.com>
2008-02-11 13:05:12 +0100

    Update blockquote specs to use "dedent" helper
    While making this change I also discovered and fixed a couple of example
    blocks in which I was setting up test data but never making an
    assertion.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/blockquote_spec.rb
+++ spec/blockquote_spec.rb
@@ -22,27 +22,30 @@ describe Wikitext::Parser, 'standard blockquotes (">" in first column)' do
   end
 
   it 'should treat ">" in first column as a blockquote marker' do
-    @parser.parse('>foo').should == <<-END
-<blockquote>
-  <p>foo</p>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo</p>
+      </blockquote>
+    END
+    @parser.parse('>foo').should == expected
   end
 
   it 'should accept (and ignore) one optional space after the ">"' do
-    @parser.parse('> foo').should == <<-END
-<blockquote>
-  <p>foo</p>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo</p>
+      </blockquote>
+    END
+    @parser.parse('> foo').should == expected
   end
 
   it 'should recognize consecutive ">" as continuance of blockquote section' do
-    @parser.parse("> foo\n> bar").should == <<-END
-<blockquote>
-  <p>foo bar</p>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo bar</p>
+      </blockquote>
+    END
+    @parser.parse("> foo\n> bar").should == expected
   end
 
   it 'should not give ">" special treatment when not on the far left' do
@@ -50,227 +53,235 @@ END
   end
 
   it 'should allow nesting of blockquotes' do
-    @parser.parse('> > foo').should == <<-END
-<blockquote>
-  <blockquote>
-    <p>foo</p>
-  </blockquote>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <blockquote>
+          <p>foo</p>
+        </blockquote>
+      </blockquote>
+    END
+    @parser.parse('> > foo').should == expected
   end
 
   it 'should allow opening of a nested blockquote after other content' do
-    @parser.parse("> foo\n> > bar").should == <<-END
-<blockquote>
-  <p>foo</p>
-  <blockquote>
-    <p>bar</p>
-  </blockquote>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo</p>
+        <blockquote>
+          <p>bar</p>
+        </blockquote>
+      </blockquote>
+    END
+    @parser.parse("> foo\n> > bar").should == expected
   end
 
   it 'should allow opening of a nested blockquote before other content' do
-    @parser.parse("> > foo\n> bar").should == <<-END
-<blockquote>
-  <blockquote>
-    <p>foo</p>
-  </blockquote>
-  <p>bar</p>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <blockquote>
+          <p>foo</p>
+        </blockquote>
+        <p>bar</p>
+      </blockquote>
+    END
+    @parser.parse("> > foo\n> bar").should == expected
   end
 
   it 'should accept an empty blockquote' do
-    @parser.parse('>').should == <<-END
-<blockquote>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+      </blockquote>
+    END
+    @parser.parse('>').should == expected
   end
 
   it 'should jump out of blockquote mode on seeing a normal line of text' do
-    @parser.parse("> foo\nbar").should == <<-END
-<blockquote>
-  <p>foo</p>
-</blockquote>
-<p>bar</p>
-END
+    expected =  dedent 6, <<-END
+      <blockquote>
+        <p>foo</p>
+      </blockquote>
+      <p>bar</p>
+    END
+    @parser.parse("> foo\nbar").should == expected
   end
 
   it 'should allow nesting of h1 blocks' do
-    @parser.parse('> = foo =').should == <<-END
-<blockquote>
-  <h1>foo</h1>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <h1>foo</h1>
+      </blockquote>
+    END
+    @parser.parse('> = foo =').should == expected
   end
 
   it 'should allow nesting of h2 blocks' do
-    @parser.parse('> == foo ==').should == <<-END
-<blockquote>
-  <h2>foo</h2>
-</blockquote>
-END
+    expected =  dedent 6, <<-END
+      <blockquote>
+        <h2>foo</h2>
+      </blockquote>
+    END
+    @parser.parse('> == foo ==').should == expected
   end
 
   it 'should allow nesting of h3 blocks' do
-    @parser.parse('> === foo ===').should == <<-END
-<blockquote>
-  <h3>foo</h3>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <h3>foo</h3>
+      </blockquote>
+    END
+    @parser.parse('> === foo ===').should == expected
   end
 
   it 'should allow nesting of h4 blocks' do
-    @parser.parse('> ==== foo ====').should == <<-END
-<blockquote>
-  <h4>foo</h4>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <h4>foo</h4>
+      </blockquote>
+    END
+    @parser.parse('> ==== foo ====').should == expected
   end
 
   it 'should allow nesting of h5 blocks' do
-    @parser.parse('> ===== foo =====').should == <<-END
-<blockquote>
-  <h5>foo</h5>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <h5>foo</h5>
+      </blockquote>
+    END
+    @parser.parse('> ===== foo =====').should == expected
   end
 
   it 'should allow nesting of h6 blocks' do
-    @parser.parse('> ====== foo ======').should == <<-END
-<blockquote>
-  <h6>foo</h6>
-</blockquote>
-END
+     expected = dedent 6, <<-END
+      <blockquote>
+        <h6>foo</h6>
+      </blockquote>
+    END
+    @parser.parse('> ====== foo ======').should == expected
   end
 
   it 'should allow alternating nested paragraphs and pre blocks' do
     # was a bug
-    input = <<-END
-> para 1
->
->  pre 1
->  pre 2
->
-> para 2
-END
-    expected = <<-END
-<blockquote>
-  <p>para 1</p>
-  <pre>pre 1
-pre 2</pre>
-  <p>para 2</p>
-</blockquote>
-END
-
+    input = dedent 6, <<-END
+      > para 1
+      >
+      >  pre 1
+      >  pre 2
+      >
+      > para 2
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>para 1</p>
+        <pre>pre 1
+      pre 2</pre>
+        <p>para 2</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should allow nesting of styled spans inside blockquotes' do
-    input = <<-END
-> Here is a link to [[something]], and some ''other''
-> `styled` '''spans'''.
-END
-    expected = <<-END
-<blockquote>
-  <p>Here is a link to <a href="/wiki/something/">something</a>, and some <em>other</em> <tt>styled</tt> <strong>spans</strong>.</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      > link to [[something]], and ''other''
+      > `styled` '''spans'''.
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>link to <a href="/wiki/something">something</a>, and <em>other</em> <tt>styled</tt> <strong>spans</strong>.</p>
+      </blockquote>
+    END
+    @parser.parse(input).should == expected
   end
 
   it 'should allow complex nestings inside blockquotes' do
     # was a bug: further reduced in the following example, "should handle TT spans inside blockquotes"
-    input = <<-END
- code block
-
-a normal para
-
-> will nest
->
->  line 1
->  line 2
->
-> other
->
-> * here
-> * is
-> * a
-> * list
->
-> outer para with ''styled''
-> `stuff` in it
->
-> > inner
-> > blockquote
-> > # inner list
-> > ## nested list
-
-follow-up para
-END
-    expected = <<-END
-<pre>code block</pre>
-<p>a normal para</p>
-<blockquote>
-  <p>will nest</p>
-  <pre>line 1
-line 2</pre>
-  <p>other</p>
-  <ul>
-    <li>here</li>
-    <li>is</li>
-    <li>a</li>
-    <li>list</li>
-  </ul>
-  <p>outer para with <em>styled</em> <tt>stuff</tt> in it</p>
-  <blockquote>
-    <p>inner blockquote</p>
-    <ol>
-      <li>inner list
-        <ol>
-          <li>nested list</li>
-        </ol>
-      </li>
-    </ol>
-  </blockquote>
-</blockquote>
-<p>follow-up para</p>
-END
+    input = dedent 6, <<-END
+       code block
+      
+      a normal para
+      
+      > will nest
+      >
+      >  line 1
+      >  line 2
+      >
+      > other
+      >
+      > * here
+      > * is
+      > * a
+      > * list
+      >
+      > outer para with ''styled''
+      > `stuff` in it
+      >
+      > > inner
+      > > blockquote
+      > > # inner list
+      > > ## nested list
+      
+      follow-up para
+    END
+    expected = dedent 6, <<-END
+      <pre>code block</pre>
+      <p>a normal para</p>
+      <blockquote>
+        <p>will nest</p>
+        <pre>line 1
+      line 2</pre>
+        <p>other</p>
+        <ul>
+          <li>here</li>
+          <li>is</li>
+          <li>a</li>
+          <li>list</li>
+        </ul>
+        <p>outer para with <em>styled</em> <tt>stuff</tt> in it</p>
+        <blockquote>
+          <p>inner blockquote</p>
+          <ol>
+            <li>inner list
+              <ol>
+                <li>nested list</li>
+              </ol>
+            </li>
+          </ol>
+        </blockquote>
+      </blockquote>
+      <p>follow-up para</p>
+    END
+    @parser.parse(input).should == expected
   end
 
   it 'should handle TT spans inside blockquotes' do
     # was a bug: this is a minimally reduced test case extracted from the integration tests
-    input = <<INPUT
-> some
-> `styled`
-INPUT
-
-      expected = <<EXPECTED
-<blockquote>
-  <p>some <tt>styled</tt></p>
-</blockquote>
-EXPECTED
-
+    input = dedent 6, <<-END
+      > some
+      > `styled`
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>some <tt>styled</tt></p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
 
   it 'should handled nested lists which immediately follow paragraphs' do
     # was a bug: this is a minimally reduced test case extracted from the integration tests
-    input = <<INPUT
-> Finally
-> # Which
-INPUT
-
-    expected = <<EXPECTED
-<blockquote>
-  <p>Finally</p>
-  <ol>
-    <li>Which</li>
-  </ol>
-</blockquote>
-EXPECTED
-
+    input = dedent 6, <<-END
+      > Finally
+      > # Which
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>Finally</p>
+        <ol>
+          <li>Which</li>
+        </ol>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
@@ -284,255 +295,249 @@ describe Wikitext::Parser, 'literal BLOCKQUOTE_START/BLOCKQUOTE_END tags' do
 
   it 'should accept literal BLOCKQUOTE_START/BLOCKQUOTE_END tags as an alternative to the standard syntax' do
     input = '<blockquote>hello</blockquote>'
-
-    expected = <<-END
-<blockquote>
-  <p>hello</p>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>hello</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
 
     # alternative 1
-    input = <<-END
-<blockquote>
-hello
-</blockquote>
-END
-
-    expected = <<-END
-<blockquote>
-  <p>hello</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+      hello
+      </blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>hello</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
 
     # alternative 2
-    input = <<-END
-<blockquote>hello
-</blockquote>
-END
-
-    expected = <<-END
-<blockquote>
-  <p>hello</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>hello
+      </blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>hello</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
 
     # alternative 3
-    input = <<-END
-<blockquote>
-hello</blockquote>
-END
-
-    expected = <<-END
-<blockquote>
-  <p>hello</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+      hello</blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>hello</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
 
     # note what happens if we indent (whitespace gets carried through; it is not identified as a PRE block
     # in reality you'd never indent when editing wikitext anyway; the idea is to free yourself from details like that
-    input = <<-END
-<blockquote>
-  hello
-</blockquote>
-END
-
-    expected = <<-END
-<blockquote>
-  <p>  hello</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+        hello
+      </blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>  hello</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should merge consecutive lines into a single paragraph' do
-    input = <<-END
-<blockquote>foo
-bar
-baz</blockquote>
-END
-    expected = <<-END
-<blockquote>
-  <p>foo bar baz</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>foo
+      bar
+      baz</blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo bar baz</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should process paragraph breaks' do
-    input = <<-END
-<blockquote>foo
-
-baz</blockquote>
-END
-    expected = <<-END
-<blockquote>
-  <p>foo</p>
-  <p>baz</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>foo
+      
+      baz</blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo</p>
+        <p>baz</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should pass through PRE tokens unaltered' do
-    input = <<-END
-<blockquote>foo
- bar</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>foo
+       bar</blockquote>
+    END
 
     # note the extra space: one for the CRLF and another for the PRE token
-    expected = <<-END
-<blockquote>
-  <p>foo  bar</p>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo  bar</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should terminate open span-level elements on hitting the newline' do
     # for now just test with EM; potentially add more examples later
-    input = <<-END
-<blockquote>foo ''bar
-baz</blockquote>
-END
-    expected = <<-END
-<blockquote>
-  <p>foo <em>bar</em> baz</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>foo ''bar
+      baz</blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo <em>bar</em> baz</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should pass through BLOCKQUOTE tokens escaped' do
-    input = <<-END
-<blockquote>foo
-> bar
-baz</blockquote>
-END
-
-    expected = <<-END
-<blockquote>
-  <p>foo &gt; bar baz</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>foo
+      > bar
+      baz</blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo &gt; bar baz</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should be able to nest single-item unordered lists' do
     input = '<blockquote>* foo</blockquote>'
-    expected = <<-END
-<blockquote>
-  <ul>
-    <li>foo</li>
-  </ul>
-</blockquote>
-END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <ul>
+          <li>foo</li>
+        </ul>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should be able to nest multi-item unordered lists' do
-    input = <<-END
-<blockquote>
-* foo
-* bar
-* baz
-</blockquote>
-END
-    expected = <<-END
-<blockquote>
-  <ul>
-    <li>foo</li>
-    <li>bar</li>
-    <li>baz</li>
-  </ul>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+      * foo
+      * bar
+      * baz
+      </blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <ul>
+          <li>foo</li>
+          <li>bar</li>
+          <li>baz</li>
+        </ul>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should be able to nest nested unordered lists' do
-    input = <<-END
-<blockquote>
-* foo
-** bar
-* baz
-</blockquote>
-END
-    expected = <<-END
-<blockquote>
-  <ul>
-    <li>foo
-      <ul>
-        <li>bar</li>
-      </ul>
-    </li>
-    <li>baz</li>
-  </ul>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+      * foo
+      ** bar
+      * baz
+      </blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <ul>
+          <li>foo
+            <ul>
+              <li>bar</li>
+            </ul>
+          </li>
+          <li>baz</li>
+        </ul>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
 
     # note that the exact placement of the closing tag doesn't matter
-    input = <<-END
-<blockquote>
-* foo
-** bar
-* baz</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+      * foo
+      ** bar
+      * baz</blockquote>
+    END
     @parser.parse(input).should == expected
 
     # likewise for the opening tag
-    input = <<-END
-<blockquote>* foo
-** bar
-* baz
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>* foo
+      ** bar
+      * baz
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should be able to nest blockquotes' do
-    input = <<-END
-<blockquote>
-foo
-<blockquote>
-bar
-</blockquote>
-baz
-</blockquote>
-END
-    expected = <<-END
-<blockquote>
-  <p>foo</p>
-  <blockquote>
-    <p>bar</p>
-  </blockquote>
-  <p>baz</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+      foo
+      <blockquote>
+      bar
+      </blockquote>
+      baz
+      </blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>foo</p>
+        <blockquote>
+          <p>bar</p>
+        </blockquote>
+        <p>baz</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
   it 'should be able to nest pre blocks' do
-    input = <<-END
-<blockquote>
-outer 1
-<pre>inner 1
-inner 2</pre>
-outer 2
-</blockquote>
-END
-    expected = <<-END
-<blockquote>
-  <p>outer 1</p>
-  <pre>inner 1
-inner 2</pre>
-  <p>outer 2</p>
-</blockquote>
-END
+    input = dedent 6, <<-END
+      <blockquote>
+      outer 1
+      <pre>inner 1
+      inner 2</pre>
+      outer 2
+      </blockquote>
+    END
+    expected = dedent 6, <<-END
+      <blockquote>
+        <p>outer 1</p>
+        <pre>inner 1
+      inner 2</pre>
+        <p>outer 2</p>
+      </blockquote>
+    END
     @parser.parse(input).should == expected
   end
 
