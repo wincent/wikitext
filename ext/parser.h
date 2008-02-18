34972c421f8b998ad7c3130ea8b4c998fffc1f86
Wincent Colaiuta <win@wincent.com>
2008-02-18 22:40:59 +0100

    Get rid of Mac OS X-specific header paths
    While including "ruby/ruby.h" will work on Mac OS X, it won't on Linux
    so change it to "ruby.h" where it should work on both. "mkmf" will take
    care of specifying the appropriate include search path.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- ext/parser.h
+++ ext/parser.h
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <ruby/ruby.h>
+#include <ruby.h>
 
 VALUE Wikitext_parser_initialize(int argc, VALUE *argv, VALUE self);
 
