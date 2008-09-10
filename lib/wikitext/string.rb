80dadc380d0b99f2f0738d5ecec4c1bffba7e3a1
Wincent Colaiuta <win@wincent.com>
2008-09-10 10:53:35 +0200

    Trim flab from string module
    Eliminate redundant whitespace and comment.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/wikitext/string.rb
+++ lib/wikitext/string.rb
@@ -15,13 +15,10 @@
 require 'wikitext'
 
 class String
-
   def to_wikitext
     @@shared_wikitext_parser ||= Wikitext::Parser.new(:space_to_underscore => true)
     @@shared_wikitext_parser.parse wikitext_preprocess
   end
-
-  # Convenience shortcut
   alias :w :to_wikitext
 
 private
@@ -31,5 +28,4 @@ private
   def wikitext_preprocess
     gsub /\b(bug|issue|request|ticket) #(\d+)/i, '[[issues/\2|\1 #\2]]'
   end
-
 end
