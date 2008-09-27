11fcab42f5a70a0aa695f86276d8f679ddc884f9
Wincent Colaiuta <win@wincent.com>
2008-09-27 21:35:58 +0200

    spec: prepend to load path to ensure development version gets tested
    We were appending to the load path so we could conceivably end up with
    a previously installed version of the extension getting tested instead
    of the just-built version, so prepend instead.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,11 +44,11 @@ end
 
 module Wikitext
   if not const_defined? 'EXTDIR'
-    # append the local "ext" directory to search path if not already present
+    # prepend the local "ext" directory to search path if not already present
     base        = File.join(File.dirname(__FILE__), '..')
     EXTDIR      = Pathname.new(File.join(base, 'ext')).realpath
     normalized  = $:.collect { |path| Pathname.new(path).realpath rescue path }
-    $:.push(EXTDIR) unless normalized.include?(EXTDIR)
+    $:.unshift(EXTDIR) unless normalized.include?(EXTDIR)
   end
 end # module Wikitext
 
