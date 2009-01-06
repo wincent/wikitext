14bf1b56bbe5e9765ce625bb4c5d3035cdf73454
Wincent Colaiuta <win@wincent.com>
2009-01-06 01:19:03 +0100

    Add specs for version module
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ spec/version_spec.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# Copyright 2009 Wincent Colaiuta
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require File.join(File.dirname(__FILE__), 'spec_helper.rb')
+require 'wikitext/version'
+
+describe Wikitext, 'versioning' do
+  it 'should provide a version string' do
+    Wikitext::VERSION.should be_instance_of(String)
+  end
+end
