b1b1b38e93db1c7b009480c897da255265c882c4
Wincent Colaiuta <win@wincent.com>
2009-03-27 12:12:46 +0100

    Work around Rails bug #2266
    See:
      https://rails.lighthouseapp.com/projects/8994/tickets/2266
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,4 @@
-# Copyright 2008 Wincent Colaiuta
+# Copyright 2008-2009 Wincent Colaiuta
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -12,6 +12,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'wikitext/nil_class'
-require 'wikitext/string'
-require 'wikitext/rails'
+# Avoid Rails bug #2266 by not requiring during "rake gems:build"
+# See: https://rails.lighthouseapp.com/projects/8994/tickets/2266
+unless $gems_build_rake_task
+  require 'wikitext/nil_class'
+  require 'wikitext/string'
+  require 'wikitext/rails'
+end
