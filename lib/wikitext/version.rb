829c04a3180a99747d06957cb6e7dfd788bc3686
Wincent Colaiuta <win@wincent.com>
2013-02-16 12:09:07 -0800

    Prepare for 3.1 release
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- lib/wikitext/version.rb
+++ lib/wikitext/version.rb
@@ -1,4 +1,4 @@
-# Copyright 2007-2012 Wincent Colaiuta. All rights reserved.
+# Copyright 2007-2013 Wincent Colaiuta. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -22,5 +22,5 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 module Wikitext
-  VERSION = '3.0.1.99'
+  VERSION = '3.1'
 end # module Wikitext
